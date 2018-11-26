410398fe285a2870c63d4fb53d170c170f0a8c41
Me-Acuerdas <44125756+Me-Acuerdas@users.noreply.github.com>
2018-11-26 12:03:24 -0900

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,31 @@
+public class Node{
+  private double weight;
+  private double input;
+  private double output;
+
+  public double getWeight(){
+    return weight;
+  }//End getWeight
+
+  public double getInput(){
+    return input;
+  }//End getInput
+
+  public double getOutput(){
+    return output;
+  }//End getOutput
+
+  public void setWeight(double value){
+    weight = value;
+  }//End setWeight
+
+  public void setInput(double value){
+    input = value;
+  }//End setWeight
+
+  public void setOutput(double value){
+    output = value;
+  }//End setWeight
+
+    
+}//End Class
