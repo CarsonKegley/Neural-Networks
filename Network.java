66bbf46b1f6243bf13a66ab40e5b2761a62ed1ef
Me-Acuerdas <44125756+Me-Acuerdas@users.noreply.github.com>
2018-12-08 15:19:02 -0900

    Add files via upload

--- Network.java
+++ Network.java
@@ -2,4 +2,26 @@ public class Network{
   private int numberOfLayers;
   private Layer[] layerArray;
 
+  public Network(int value){
+    numberOfLayers = value;
+    layerArray= new Layer[value];
+  }//End constructor
+
+  public int getNumberOfLayers(){
+    return numberOfLayers;
+  }//End Getter
+
+  public void setNumberOfLayers(int value){
+    numberOfLayers = value;
+  }//End setter
+
+  public Layer[] getLayerArray(int value){
+    layerArray[value];
+  }//End Getter
+
+  public void addLayer(int value){
+    layerArray[value] = new Layer();
+  }//End addLayer
+
+
 }//End Class
